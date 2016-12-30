c5f8d9c05939cc1b98ac8a7ea51a8df59524010e
randomdeathbymusic <mallen@v0lture.com>
2016-12-29 18:56:37 -0700

    Register support
    Supplied username, password, and email now registers user

--- accounts/user.php
+++ accounts/user.php
@@ -10,17 +10,59 @@
   require_once "../secrets.php";
   global $db;
 
+  // clear empty vars
+  $result = NULL;
+  $error = NULL;
+
   // class inits
   $telemetry = new Telemetry($db);
   $auth = new Auth($db, $telemetry);
   $user = new User($auth, $db, $telemetry);
 
-  // run
-  if(isset($_POST["token"])) {
-    $t = $_POST["token"];
-    $resp = $user->getAuthUser($t);
+  // get mode
+  if(isset($_POST["mode"])) {
+    $m = $_POST["mode"];
+
+    // get user based on token
+    if($m == "getUserAuth") {
+      // run
+      if(isset($_POST["token"])) {
+        $t = $_POST["token"];
+        $resp = $user->getAuthUser($t);
+      } else {
+        $error = "missing_vars";
+      }
+
+    // is user
+    } elseif($m == "isUser") {
+      if(isset($_POST["username"])) {
+        $u = $_POST["username"];
+        $resp2 = $user->isUser($u);
+        if(isset($resp2["error"])) {
+          $resp = $resp2;
+        } else {
+          $resp["data"] = $resp2["isuser"];
+        }
+      } else {
+        $error = "missing_vars";
+      }
+
+    // register user
+    } elseif($m == "register") {
+      if(isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"])) {
+        $u = $_POST["username"];
+        $p = $_POST["password"];
+        $e = $_POST["email"];
+        $resp2 = $user->register($u, $p, $e);
+        if(isset($resp2["error"])) {
+          $resp = $resp2;
+        } else {
+          $resp["data"] = $resp2["created"];
+        }
+      }
+    }
   } else {
-    $error = "missing_vars";
+    $error = "missing_mode";
   }
 
   // conditionals with error status
--- library/user.php
+++ library/user.php
@@ -1,5 +1,6 @@
 <?php
 
+  require_once "parse.php";
 
   class User {
 
@@ -16,6 +17,9 @@
 
     // get authenticated user
     public function getAuthUser($token) {
+
+      // clean vars
+      $token = filter($token);
 
       // check token status
       $ts = $this->auth->validateToken($token);
@@ -83,6 +87,117 @@
           return Array ("logged" => "no: ".$t, "error" => "associate_invalid_token:".$ts['error']);
         }
       }
+    }
+
+    // is user
+    public function isUser($user) {
+      $user = filter($user);
+
+
+      // query db
+      if($rd = $this->dbc->query("SELECT * FROM `accounts` WHERE username = '".$user."'")) {
+
+        // count results
+        if($rd->num_rows == 1) {
+          $t = $this->telemetry->user("isuser", $user);
+          if($t == "success") {
+            return Array("logged" => "yes", "isuser" => true);
+          } else {
+            return Array("logged" => "no: ".$t, "isuser" => true);
+          }
+        } elseif($rd->num_rows > 1) {
+          // duplicate accounts
+          $t = $this->telemetry->error("account_dupe", "user.php > isUser()", "Queried '".$user."'");
+          if($t == "success") {
+            return Array("logged" => "yes", "error" => "account_dupe");
+          } else {
+            return Array("logged" => "no: ".$t, "error" => "account_dupe");
+          }
+        } else {
+          // :( we couldn't find anything boss
+          $t = $this->telemetry->user("isuser_failed", $user);
+          if($t == "success") {
+            return Array("logged" => "yes", "isuser" => false);
+          } else {
+            return Array("logged" => "no: ".$t, "isuser" => false);
+          }
+        }
+
+      } else {
+        // log error and respond back
+        $t = $this->telemetry->error("isuser_query", "user.php > isUser() > query", $this->dbc->query);
+        if($t == "success") {
+          return Array("logged" => "yes", "error" => "isuser_query");
+        } else {
+          return Array("logged" => "no: ".$t, "error" => "isuser_query");
+        }
+      }
+    }
+
+    // create user
+    public function register($user, $pass, $email) {
+
+      // clean vars
+      $user = filter($user);
+      $pass = filter(password_hash($pass, PASSWORD_DEFAULT));
+      $email = filter($email);
+
+      // check if isUser
+      $iU = $this->isUser($user);
+      if($iU["isuser"] == true) {
+        // respond username taken
+        $t = $this->telemetry->error("register_usertaken", "user.php > register() > isUser()");
+        if($t == "success") {
+          return Array("logged" => "yes", "error" => "registerusertaken");
+        } else {
+          return Array("logged" => "no: ".$t, "error" => "registerusertaken");
+        }
+      } elseif(isset($iU["error"])) {
+        // an error occurred
+        $t = $this->telemetry->error("register_isuser:".$iU["error"], "user.php > register() > isUser()");
+        if($t == "success") {
+          return Array("logged" => "yes", "error" => "register_isuser:".$iU["error"]);
+        } else {
+          return Array("logged" => "no: ".$t, "error" => "register_isuser:".$iU["error"]);
+        }
+      } else {
+        // proceed with register
+
+        // create tags
+        $logtag = filter($this->createTag());
+        $tokentag = filter($this->createTag());
+
+        // insert
+        if($this->dbc->query("INSERT INTO `accounts` (`id`, `username`, `email`, `password`, `frozen`, `2fa`, `secret`, `logtag`, `tokentag`) VALUES (NULL, '".$user."', '".$email."', '".$pass."', 0, 0, '', '".$logtag."', '".$tokentag."')")) {
+          // creation successful
+          $t = $this->telemetry->user("create", $user);
+          if($t == "success") {
+            return Array("logged" => "yes", "created" => true);
+          } else {
+            return Array("logged" => "no: ".$t, "created" => true);
+          }
+        } else {
+          // error
+          $t = $this->telemetry->error("register_failed", "user.php > register() > isUser() > create query", $this->dbc->error);
+          if($t == "success") {
+            return Array("logged" => "yes", "error" => "register_failed");
+          } else {
+            return Array("logged" => "no: ".$t, "error" => "register_failed");
+          }
+        }
+      }
+
+    }
+
+    // tag creation
+    private function createTag() {
+      $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+      $str = '';
+      $max = mb_strlen($keyspace, '8bit') - 1;
+      for ($i = 0; $i < 50; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+      }
+      return $str;
     }
 
   }
