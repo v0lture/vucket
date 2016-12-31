a0ea51beb514e28d9f920d40b242704dae1e3568
randomdeathbymusic <mallen@v0lture.com>
2016-12-30 19:56:11 -0700

    Added MySQL for Travis CI to Parse

--- library/telemetry.php
+++ library/telemetry.php
@@ -7,12 +7,36 @@
   class Telemetry {
 
     // database connector
-    private $db;
+    private $dbc;
 
     public function __construct($db) {
       // set $db
       $this->dbc = $db;
     }
+
+    // Function logging
+    // Monitors function run times and their state
+    // States: success, degraded, error
+    // Meant to be a "try-and-see" analytic, result is typically ignored unless needed
+
+    public function functionLog($state, $func, $logid) {
+      // init vars
+      $state = filter($state);
+      $func = filter($func);
+      $logid = filter($logid);
+      $time = filter(microtime(true));
+
+      // submit to database
+      if($this->dbc->query("INSERT INTO `log` (id, time, state, func, logid) VALUES (NULL, '".$time."', '".$state."', '".$func."', '".$logid."')")){
+        return "success";
+      } else {
+        return "error: ".$this->dbc->error;
+      }
+    }
+
+    // Event logging
+    // Logs all the events
+    // Logs event type, trace/data, mysql error, or other relative data
 
     // error logging
     public function error($code, $trace, $mysql="none") {
@@ -25,9 +49,9 @@
 
       // submit to database
       if($this->dbc->query("INSERT INTO `log` (id, code, trace, mysql, ip, time, type) VALUES (NULL, '".$code."', '".$trace."', '".$mysql."', '".$ip."', '".$time."', 'error')")){
-        return "success";
+        return Array("id" => $this->dbc->insert_id, "d" => "success");
       } else {
-        return "error: ".$this->dbc->error;
+        return Array("id" => $this->dbc->insert_id, "d" => "error: ".$this->dbc->error);
       }
     }
 
@@ -42,11 +66,11 @@
 
       // submit to database
       if($this->dbc->query("INSERT INTO `log` (id, code, trace, mysql, ip, time, type) VALUES (NULL, '".$code."', '".$username."', '".$mysql."', '".$ip."', '".$time."', 'auth')")){
-        return "success";
+        return Array("id" => $this->dbc->insert_id, "d" => "success");
       } else {
         // logception
         $this->error("telemetry_auth", "telemetry.php > auth()", $this->dbc->error);
-        return "error: ".$this->dbc->error;
+        return Array("id" => $this->dbc->insert_id, "d" => "error: ".$this->dbc->error);
       }
     }
 
@@ -61,11 +85,11 @@
 
       // submit to database
       if($this->dbc->query("INSERT INTO `log` (id, code, trace, mysql, ip, time, type) VALUES (NULL, '".$method."', '".$token."', '".$mysql."', '".$ip."', '".$time."', 'token')")){
-        return "success";
+        return Array("id" => $this->dbc->insert_id, "d" => "success");
       } else {
         // logception
-        $this->error("telemetry_auth", "telemetry.php > auth()", $this->dbc->error);
-        return "error: ".$this->dbc->error;
+        $this->error("telemetry_auth", "telemetry.php > token()", $this->dbc->error);
+        return Array("id" => $this->dbc->insert_id, "d" => "error: ".$this->dbc->error);
       }
     }
 
@@ -80,11 +104,11 @@
 
       // submit to database
       if($this->dbc->query("INSERT INTO `log` (id, code, trace, mysql, ip, time, type) VALUES (NULL, '".$action."', '".$logtag."', '".$mysql."', '".$ip."', '".$time."', 'user')")){
-        return "success";
+        return Array("id" => $this->dbc->insert_id, "d" => "success");
       } else {
         // logception
-        $this->error("telemetry_auth", "telemetry.php > auth()", $this->dbc->error);
-        return "error: ".$this->dbc->error;
+        $this->error("telemetry_auth", "telemetry.php > user()", $this->dbc->error);
+        return Array("id" => $this->dbc->insert_id, "d" => "error: ".$this->dbc->error);
       }
     }
 
