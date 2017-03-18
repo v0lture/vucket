075887931d99929676cc179e277f90535de98a8b
Amber <mallen@v0lture.com>
2017-03-18 10:14:13 -0600

    First attempted fix for failing tests
    Job #39 is now failing due to PHPunit errors though there were no code base changes from the commit initiating it.

--- tests.php
+++ tests.php
@@ -10,7 +10,7 @@
   require_once "library/vat.php";
   require_once "library/cycles.php";
 
-  class tests extends PHPUnit_Framework_TestCase {
+  class PHPUnit_Framework_TestCase {
 
     public function test() {
 
